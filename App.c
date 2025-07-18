7e83cbb33ac19bc69f774080779ea7bd443881a0
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-07-19 07:27:56 +0900

    Update App.c

--- App.c
+++ App.c
@@ -2,18 +2,20 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_stdinc.h>
 #include "Physics/Particle.c"
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-static float prev_millisec = 0.0f;
+static const double MILLISECS_PER_FRAME = 1/120 * 1000;
+static Uint64 prev_ticks = 0;
 static const float particle_size = 50.0f;
 
 Particle particle = { 
 	.position = { .x = 250, .y = 120 }, 
-	.velocity = { .x = 0, .y = 0 } 
+	.velocity = { .x = 50, .y = 0 } 
 };
 
 /* This function runs once at startup. */
@@ -46,17 +48,23 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-	const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
-	const float delta_time = now - prev_millisec;
+	
+	Uint64 current_ticks = SDL_GetTicks();
+	int timeToWait = MILLISECS_PER_FRAME - (current_ticks - prev_ticks);
+	if (timeToWait > 0) {
+		SDL_Delay(timeToWait);
+	}
+	double delta_time = (current_ticks - prev_ticks) / 1000.0f;
 
 	//adding velocity per frame
 	//todo: adding velocity per second. cuz it makes more sense. 
 	//meter / sec 
 	
-	particle.position = Vec2_Add(particle.position,Vec2_Scale( particle.velocity,delta_time ));
+	Vector2 added_pos = Vec2_Scale(particle.velocity, delta_time);
+	particle.position = Vec2_Add(particle.position, added_pos);
 
 
-	prev_millisec = now; 
+	prev_ticks = current_ticks; 
 
 	/* clear the window to the draw color. */
 	SDL_SetRenderDrawColor(renderer,0,0,0,SDL_ALPHA_OPAQUE);
@@ -64,8 +72,21 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
 	const SDL_FRect rect = { .x = particle.position.x, .y = particle.position.y, .w = particle_size, .h = particle_size };
 
-	SDL_SetRenderDrawColor(renderer,0xFF,0x00,0x00,SDL_ALPHA_OPAQUE);
-	SDL_RenderFillRect(renderer, &rect);
+	SDL_SetRenderDrawColor(renderer,0x00,0xFF,0x00,SDL_ALPHA_OPAQUE);
+
+	//draw circle with render lines
+	{
+		int cx = particle.position.x;
+		int cy = particle.position.y;
+		float radius = 15.0f;
+		for (int dy = -radius; dy <= radius; ++dy) {
+			int dx = (int)(SDL_sqrtf(radius * radius - dy * dy));
+			int x1 = cx - dx;
+			int x2 = cx + dx;
+			SDL_RenderLine(renderer, x1, cy + dy, x2, cy + dy);
+		}
+	}
+
 
 	/* put the newly-cleared rendering on the screen. */
 	SDL_RenderPresent(renderer);
